266d77c94ec1f3d211bc1d2dbb5409b51e347828
karrirasinmaki <karri.rasinmaki@gmail.com>
2015-01-25 14:07:38 +0200

    Task list db

--- js/db.js
+++ js/db.js
@@ -61,6 +61,16 @@
 	}
 	
 	/**
+	 * Return how many hours this task 
+	 * will take of day.
+	 */
+	function tasksDailyHours(doc) {
+		var hoursLeft = (doc.estimateHours || 0) - (doc.userdHours || 0);
+		return Math.min(hoursLeft, 4);
+	}
+	window.TodAi.tasksDailyHours = tasksDailyHours;
+	
+	/**
 	 * Get given dates date at 00:00:00 o'clock. If no 
 	 * date given use today's date.
 	 */
@@ -68,7 +78,8 @@
 		var date;
 		if (dateval) date = new Date(dateval);
 		else date = new Date();
-		
+
+		date.setMilliseconds(0);
 		date.setSeconds(0);
 		date.setMinutes(0);
 		date.setHours(0);
@@ -135,18 +146,85 @@
 			db.query({map: map}, {reduce: false}, callback);
 		},
 		
+		/**
+		 * Return date of list. If following rules match, request new 
+		 * today's list and update this info data. Also provide 
+		 * isNewDate=true.
+		 *
+		 * Rule:
+		 *   there is no date record || 
+		 *   date of list < today &&
+		 *   (task list is empty || today it's over 5am)
+		 *   
+		 */
+		dateOfList: function(callback) {
+			var self = this;
+			var id = "date-of-list";
+			function returnNewDay() {
+				self.requestNewTodaysTaskList(function(){});
+			}
+			function isItTimeForDayChange(doc, callback) {
+				if (dateToday(doc.date) < dateToday()) {
+					if ((new Date()).getHours() < 5) {
+						this.listTodaysTasks(function(err, resp) {
+							if (err) {
+								callback(true);
+								return;
+							}
+							else {
+								callback(resp.rows.length <= 0);
+							}
+						});
+					}
+					else {
+						callback(true);
+						return;
+					}
+				}
+				callback(false);
+			}
+			db.get(id, function(err, resp) {
+				if (err) {
+					returnNewDay();
+				}
+				else {
+					isItTimeForDayChange(resp, function(bool) {
+						if (bool) {
+							resp.date = dateToday();
+							returnNewDay(resp);
+						}
+						else {
+							callback(err, resp);
+						}
+					});
+				}
+			});
+		},
+		
+		updateDateOfList: function(callback) {
+			var id = "date-of-list";
+			db.get(id, function(err, resp) {
+				if (!resp) {
+					resp = {_id: id, date: dateToday()};
+				}
+				resp.date = dateToday();
+				db.put(resp, function() {
+					resp.isNewDay = true;
+					callback(null, resp);
+				});
+			});
+		},
 		
 		/**
 		 * Generate new today's tasklist.
 		 */
 		requestNewTodaysTaskList: function(callback) {
+			this.updateDateOfList(function(){});
 			function map(doc) {
-				var hoursLeft = doc.estimateHours - doc.userdHours;
-				
-				if (doc.type === "todo" && 
-					(new Date(doc.deadline) > window.TodAi.dateToday() || hoursLeft > 0)) {
+				var hoursLeft = (doc.estimateHours || 0) - (doc.userdHours || 0);
+				if (doc.type === "todo" && hoursLeft > 0) {
 					
-					emit([doc.deadline, -hoursLeft], doc._id);
+					emit([doc.deadline, -hoursLeft], doc);
 				}
 			}
 			db.query({map: map}, {reduce: false, limit: 4}, function(err, resp) {
@@ -154,20 +232,35 @@
 					callback(err, resp);
 					return;
 				}
+				var toUpdate = [];
+				var totalHours = 0;
 				for (var i = 0, l = resp.rows.length; i < l; ++i) {
-					var row = resp.rows[i];
-					row.showDate = app.dateToday();
+					var doc = resp.rows[i].value;
+					var hours = tasksDailyHours(doc);
+					if (totalHours + hours <= 8) {
+						doc.showDate = app.dateToday();
+						doc.dailyHours = hours;
+						toUpdate.push(doc);
+						totalHours += hours;
+					}
 				}
-				db.bulkDocs(resp.rows, callback);
+				db.bulkDocs(toUpdate, callback);
 			});
 		},
 		
 		/**
 		 * List today's tasks.
 		 */
-		listTodaysTasks: function(callback) {
+		listTodaysTasks: function(callback, skipDateCheck) {
+			var self = this;
+			if (!skipDateCheck) {
+				this.dateOfList(function() {
+					self.listTodaysTasks(callback, true);
+				});
+				return;
+			}
 			function map(doc) {
-				var hours = Math.min(doc.estimateHours, 4);
+				var hours = doc.dailyHours;
 				
 				if (doc.type === "todo" && doc.showDate != 0 && window.TodAi.dateToday(doc.showDate) <= window.TodAi.dateToday()) {
 					emit([doc.deadline, hours], {
@@ -178,20 +271,6 @@
 				}
 			}
 			db.query({map: map}, {reduce: false, limit: 4}, function(err, resp) {
-				if (err) {
-					callback(err, resp);
-					return;
-				}
-				var totalHours = 0;
-				for (var i = 0, l = resp.rows.length; i < l; ++i) {
-					var row = resp.rows[i];
-					if (totalHours > 8) {
-						delete resp.rows[i];
-					}
-					else {
-						totalHours += row.value.hours;
-					}
-				}
 				callback(err, resp);
 			});
 		}
